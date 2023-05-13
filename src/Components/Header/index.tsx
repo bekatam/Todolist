fa4f862aabfebd8230d10d5aefa302150475e505
bekatam <1bekatam@gmail.com>
2023-05-13 09:40:54 +0600

    added quote generation

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,10 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import './Header.css';
+import LoopIcon from '@mui/icons-material/Loop';
 
 export default function Header() {
-    return (
-        <>
-            <div className="header__title">Simple To Do List</div>
-            <div className="header__subtitle">Today is awesome day. The weather is awesome, you are awesome too!</div>
-        </>
-    )
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    const getQuote = async () => {
+      const response = await fetch('https://type.fit/api/quotes');
+      const data = await response.json();
+      const randomNumber = Math.floor(Math.random() * data.length);
+      setQuote(data[randomNumber].text)
+    };
+
+    const timeout = setTimeout(getQuote, 3000);
+    return () => clearTimeout(timeout); 
+  }, []);
+
+  return (
+    <>
+      <div className="header__title">Simple To Do List</div>
+      <div className="header__subtitle">{quote ? quote : <><LoopIcon/>Loading quote...</>}</div>
+    </>
+  );
 }
